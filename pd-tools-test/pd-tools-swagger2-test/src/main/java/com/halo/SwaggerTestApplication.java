04c657d6e91d8c7646bc044b285a93c352d27a7d
lanqilu <885240677@qq.com>
2021-11-12 14:36:19 +0800

    添加 pd-tools-swagger2 的使用演示 Demo

---
+++ pd-tools-test/pd-tools-swagger2-test/src/main/java/com/halo/SwaggerTestApplication.java
@@ -0,0 +1,16 @@
+package com.halo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Halo
+ * @create 2021/11/12 下午 02:33
+ * @description
+ */
+@SpringBootApplication
+public class SwaggerTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerTestApplication.class, args);
+    }
+}
